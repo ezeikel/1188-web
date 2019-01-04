fb1172e9bef034d923db9ab1f4427b14bee782d4
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2019-01-04 21:26:02 +0000

    Team page layout done.

--- pages/who-we-are.js
+++ pages/who-we-are.js
@@ -20,7 +20,6 @@ const Title = styled.h2`
 const TeamCards = styled.section`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, auto));
-  grid-auto-rows: 600px;
   grid-auto-flow: dense;
   justify-items: center;
   grid-gap: var(--spacing-large);
@@ -28,12 +27,12 @@ const TeamCards = styled.section`
 `;
 
 const TeamCard = styled.div`
-  display: grid;
-  grid-template-rows: auto 1fr 1fr;
-  grid-row-gap: var(--spacing-small);
+  display: flex;
+  flex-direction: column;
 `;
 
 const Photo = styled.img`
+  flex: 8 1 auto;
   max-height: 100%;
   width: 100%;
   object-fit: cover;
@@ -41,47 +40,79 @@ const Photo = styled.img`
 `;
 
 const Name = styled.span`
+  flex: 1 1 auto;
   display: grid;
   justify-items: start;
   align-items: center;
+  margin-top: var(--spacing-medium);
+  font-weight: normal;
+  color: var(--color-black);
 `;
 
 const Role = styled.span`
+  flex: 1 1 auto;
   display: grid;
   justify-items: start;
   align-items: center;
+  font-weight: lighter;
+  color: var(--color-black);
 `;
 
 const team = [
   {
-    name: 'Ezeikel Pemberton',
-    role: 'Founder & CTO',
-    image: '/static/images/ezeikel-pemberton.jpg'
+    name: "Ezeikel Pemberton",
+    role: "Founder & Creative Director",
+    image: "/static/images/ezeikel-pemberton.jpg"
   },
   {
-    name: 'Ezeikel Pemberton',
-    role: 'Founder & CTO',
-    image: '/static/images/ezeikel-pemberton.jpg'
+    name: "Karen Hollywood",
+    role: "Co-founder / Front-end Development",
+    image: "/static/images/ezeikel-pemberton.jpg"
   },
   {
-    name: 'Ezeikel Pemberton',
-    role: 'Founder & CTO',
-    image: '/static/images/ezeikel-pemberton.jpg'
+    name: "Lee Karolczak",
+    role: "Development Lead",
+    image: "/static/images/ezeikel-pemberton.jpg"
   },
   {
-    name: 'Ezeikel Pemberton',
-    role: 'Founder & CTO',
-    image: '/static/images/ezeikel-pemberton.jpg'
+    name: "Paul Spencer",
+    role: "Design & Front-end Development",
+    image: "/static/images/ezeikel-pemberton.jpg"
   },
   {
-    name: 'Ezeikel Pemberton',
-    role: 'Founder & CTO',
-    image: '/static/images/ezeikel-pemberton.jpg'
+    name: "Mark Gale",
+    role: "Project Manager",
+    image: "/static/images/ezeikel-pemberton.jpg"
   },
   {
-    name: 'Ezeikel Pemberton',
-    role: 'Founder & CTO',
-    image: '/static/images/ezeikel-pemberton.jpg'
+    name: "Dedy Lawidjaja",
+    role: "Web & Mobile Developer",
+    image: "/static/images/ezeikel-pemberton.jpg"
+  },
+  {
+    name: "Kylie Timpani",
+    role: "Senior Designer",
+    image: "/static/images/ezeikel-pemberton.jpg"
+  },
+  {
+    name: "Sandy Lim",
+    role: "Words + Strategy",
+    image: "/static/images/ezeikel-pemberton.jpg"
+  },
+  {
+    name: "Matt Boddy",
+    role: "Web Developer",
+    image: "/static/images/ezeikel-pemberton.jpg"
+  },
+  {
+    name: "Teegan Lincoln",
+    role: "Front-end Development",
+    image: "/static/images/ezeikel-pemberton.jpg"
+  },
+  {
+    name: "Callum Shand",
+    role: "Designer",
+    image: "/static/images/ezeikel-pemberton.jpg"
   }
 ];
 
