0f19e0c5912870b81cd60bbb502ca4f0f332b3f0
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2019-12-22 00:34:11 +0000

    More jest updates.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 module.exports = {
   testEnvironment: "jest-environment-jsdom",
+  moduleDirectories: ["node_modules", path.join(__dirname, "src")],
   moduleNameMapper: {
     "\\.css$": require.resolve("./test/style-mock.js"),
   },
